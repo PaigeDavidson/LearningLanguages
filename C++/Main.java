ce97649fd41bb45594ce9dd1c879463d74ee750d
Paige <davidsonpaige22@gmail.comgit>
2025-01-09 11:04:49 -0700

    c++

---
+++ C++/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int j = 0; j < 7; j++) {
+            for (int k = 0; k < j; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
